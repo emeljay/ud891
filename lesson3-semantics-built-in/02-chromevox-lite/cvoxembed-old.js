e3faf31b3df2834629128f02f3a70e098af5498d
Melissa Johnson <melissaleighjohnson@gmail.com>
2018-07-26 03:40:11 -0700

    unused dom gen

---
+++ lesson3-semantics-built-in/02-chromevox-lite/cvoxembed-old.js
@@ -0,0 +1,96 @@
+// buildShade() {
+//       const shade = document.createElement('div');
+//   shade.class = this.getClass('shade');
+//   shade.id = this.getId('shade');
+//   shade.ariaHidden = true;
+//   return shade;
+// }
+
+// buildCvoxLogo() {
+//     const logo = document.createElement('span');
+//     logo.id = this.getId('cvox-logo');
+//     const imgEnabled = document.createElement('img');
+//     imgEnabled.height = 24;
+//     imgEnabled.width = 24;
+//     imgEnabled.src = 'chromevox-no-background.svg';
+//     imgEnabled.alt = 'ChromeVox Lite logo';
+//     imgEnabled.class = 'enabled';
+
+//     const imgDisabled = document.createElement('img');
+//     imgEnabled.
+//     <span id="cvox-logo">
+//     <img height="24" width="24" src="chromevox-no-background.svg" alt="ChromeVox Lite logo" class="enabled">
+//     <img height="24" width="24" src="chromevox-no-background-off.svg" alt="ChromeVox Lite disabled logo" class="disabled">
+//   </span>
+// }
+
+// buildCaption() {
+//   <span id="caption"></span>
+// }
+
+// buildEnableDisableButton() {
+//     <button id="toggle-cvox"><span id="enable-cvox">Enable ChromeVox Lite</span><span id="disable-cvox">Disable ChromeVox Lite</button>
+// }
+
+// buildCvoxEmbed() {
+//     <div id="cvoxembed" class="cvoxembed" aria-hidden="true">
+// }
+
+// buildCvoxEmbedContent() {
+//     <div class="content">
+
+// }
+
+// buildCvoxEmbedControls() {
+//     <div class="controls">
+// }
+
+// buildVoices() {
+//      <span>
+//         Voice:
+//         <select id="voices">
+//         </select>
+//       </span>
+// }
+
+
+// buildSpeechRates() {
+//     <span>
+//         Speech rate:
+//         <select id="rates">
+//           <option value="1.0">Slow</option>
+//           <option value="1.2" selected>Default</option>
+//           <option value="1.8">Intermediate</option>
+//           <option value="3.0">
+//               Advanced (normal for a blind person)
+//           </option>
+//         </select>
+//       </span>
+// }
+
+// buildNavigation() {
+//     <span>
+//         Navigate:
+//         <button id="previous">Previous</button>
+//         <button id="next">Next</button>
+//         <button id="click">Click</button>
+//         <button id="heading">Heading</button>
+//         <button id="top">Top</button>
+//       </span>
+// }
+
+// buildVolume() {
+//     <span class="volume">
+//         Volume: <input id="volume" type="range" min=0 value=100 max=100>
+//       </span>
+// }
+
+
+
+// getId(name) {
+//     return `cve-${name}`;
+// }
+
+// getClass(name) {
+// 	return `cve-${name}`;
+// }
